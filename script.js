40b052399a43ede73520b23cfe7af0037b8eba21
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2015-11-13 13:57:20 -0700

    made full page of pixels, allowed paintbrush colors red and purple

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-//add event listener to each div that changes background color to red upon "click"
-
 var paintingCanvas = document.querySelector("#paintingCanvas");
-// var pixels = document.querySelector(".square");
-paintingCanvas.addEventListener("click", function (event) {
-  if (event.target.style.backgroundColor === "white") {
-    event.target.style.backgroundColor = "red";
-  } else {
-    event.target.style.backgroundColor = "white";
-  }
-});
-
 //add event listener that sets paintbrush color
+var colorPalette = document.querySelector("#palette");
+colorPalette.addEventListener("click", function (event)  {
+  if (event.target.style.backgroundColor === "red") {
+    paintingCanvas.addEventListener("click", function (event) {
+      if (event.target.style.backgroundColor !== "red") {
+        event.target.style.backgroundColor = "red";
+      }
+    })
+  } else if (event.target.style.backgroundColor === "purple") {
+      paintingCanvas.addEventListener("click", function (event) {
+          event.target.style.backgroundColor = "purple";
+        });
+  };
+});
 
 //add event listener that changes divs upon click
